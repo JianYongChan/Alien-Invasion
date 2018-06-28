4b383f093bc1c5604b379f180ba0e606f991329e
lelouch <lelouch.279978955@gmail.com>
2018-06-28 21:25:54 +0800

    display a ship in the screen

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -2,10 +2,20 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 def run_game():
+
+    # 初始化游戏并创建一个screen对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     while True:
 
@@ -14,10 +24,11 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
 
 run_game()
-
-
